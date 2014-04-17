34696d12226c298a5e9addb644e41c2a3557627d
Mingli Yuan <mingli.yuan@gmail.com>
2014-04-17 17:06:32 +0800

    rewrite totally: remove reflection and customized classloading

--- src/main/java/com/guokr/nlp/seg/SegWrapper.java
+++ src/main/java/com/guokr/nlp/seg/SegWrapper.java
@@ -1,45 +1,33 @@
 package com.guokr.nlp.seg;
 
-import java.util.Properties;
-
-import com.guokr.util.Reflector;
+import com.guokr.protocol.Protocols;
+import com.guokr.util.Settings;
 
 import edu.stanford.nlp.ie.crf.CRFClassifier;
 
 public class SegWrapper {
 
-    private static Class<?>  clzSettings = Reflector.classFor("com.guokr.util.Settings");
-    private static Object    defaults    = Reflector.callStaticly(clzSettings, "load", new Class[] { String.class },
-                                                 new Object[] { "xcf:///seg/defaults.using.prop" });
+    static Protocols         protocols = Protocols.INSTANCE;
+    static Settings          defaults  = Settings.load("xcf:///seg/defaults.using.prop");
 
     private CRFClassifier<?> classifier;
 
-    public SegWrapper(Object settings) {
-        Properties props = (Properties) Reflector.newInstance(clzSettings, new Class[] { Properties.class,
-                Properties.class }, new Object[] { settings, defaults });
+    public SegWrapper() {
+        this(null);
+    }
 
+    public SegWrapper(Settings settings) {
+        Settings props = new Settings(settings, defaults);
         String model = props.getProperty("model");
         try {
-            classifier = CRFClassifier.getClassifier(model, props);
-
-            // ObjectOutputStream oos = new ObjectOutputStream(new
-            // GZIPOutputStream(new MultipleGZIPOutputStream(
-            // "/Users/mingli/git/stan-cn-seg/src/main/resources/seg/ctb/ctb.gz")));
-            // classifier.serializeClassifier(oos);
-            // oos.flush();
-            // oos.close();
+            this.classifier = CRFClassifier.getClassifier(model, props);
         } catch (Exception e) {
-            System.out.println(e);
             e.printStackTrace(System.err);
         }
     }
 
-    public SegWrapper() {
-        new SegWrapper((Object)null);
-    }
-
     public String segment(String text) {
-        return classifier.classifyToString(text).trim();
+        return this.classifier.classifyToString(text).trim();
     }
 
 }
