fce6342c8671db910e676028b0edc8cba6404fd8
jseagull <seagull@seagull-guokr.(none)>
2014-07-09 12:56:18 +0800

    migrate to stanford-corenlp 3.4

---
+++ src/main/java/com/guokr/nlp/seg/SegMigrate.java
@@ -0,0 +1,29 @@
+package com.guokr.nlp.seg;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import com.guokr.util.MultipleGZIPOutputStream;
+
+public class SegMigrate {
+
+	public static void main(String[] args) {
+		try {
+			MultipleGZIPOutputStream mgzo = new MultipleGZIPOutputStream(
+					args[1]);
+			GZIPOutputStream gos = new GZIPOutputStream(mgzo);
+			GZIPInputStream fis = new GZIPInputStream(new FileInputStream(
+					args[0]));
+			while (fis.available() != 0) {
+				gos.write(fis.read());
+			}
+			fis.close();
+			gos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
